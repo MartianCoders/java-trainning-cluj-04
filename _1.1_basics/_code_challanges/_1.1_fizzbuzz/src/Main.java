a314af34a88faea3c4088068a00b84bef31db677
Teodor Ganga <info.teodor@gmail.com>
2020-04-19 14:37:56 +0300

    added last challanges

--- _1.1_basics/_code_challanges/_1.1_fizzbuzz/src/Main.java
+++ _1.1_basics/_code_challanges/_1.1_fizzbuzz/src/Main.java
@@ -1,13 +1,15 @@
-
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        fizzBuzz();
+        //fizzBuzz();
+        //FooBarQix();
+        //Pair2();
+        Pair3();
     }
 
     public static void fizzBuzz() {
         StringBuilder finalString = new StringBuilder();
-        boolean checkIfDiv = false;
 
         FizzBuzz[] elements = {
                 new FizzBuzz(3,"Fizz"),
@@ -17,6 +19,7 @@ public class Main {
         };
 
         for(short i = 1; i <= 100; i++) {
+            boolean checkIfDiv = false;
             for (FizzBuzz counter : elements) {
                 if (i % counter.getNumber() == 0) {
                     checkIfDiv = true;
@@ -32,4 +35,74 @@ public class Main {
         System.out.println(finalString);
     }
 
+    public static void FooBarQix() {
+        FizzBuzz[] elements = {
+                new FizzBuzz(3, "Foo"),
+                new FizzBuzz(5,"Bar"),
+                new FizzBuzz(7,"Qix"),
+        };
+
+        for(short i = 1; i <= 30; i++) {
+            boolean checkIfDiv = false;
+            StringBuilder string = new StringBuilder();
+            string.setLength(0);
+            for (FizzBuzz counter : elements) {
+                if (i % counter.getNumber() == 0) {
+                    checkIfDiv = true;
+                    string.append(counter.getWord());
+                    int copy = i;
+                    while (copy > 0) {
+                        int digit = copy % 10;
+                        if (digit == counter.getNumber()) {
+                            string.append(counter.getWord());
+                        }
+                        copy = copy / 10;
+                    }
+                    System.out.println(string);
+                }
+            }
+            if(!checkIfDiv) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void Pair2() {
+        int[] a = { 5, 9, -5, 7, -5}; // -5, -5, 5, 7, 9
+        int finalCounter = 0;
+        int counter = 1;
+        Arrays.sort(a);
+        for(int i = 1; i < a.length; i++) {
+            if (a[i] == a[i - 1])
+                counter++;
+            else {
+                if (counter % 2 == 0)
+                    finalCounter++;
+                counter = 1;
+            }
+        }
+        if (counter % 2 == 0)
+            finalCounter++;
+        System.out.println(finalCounter);
+    }
+
+
+    public static void Pair3() {
+        int[] a = { 2, -1, -1, -1}; // -1, -1, -1, 2
+        int finalCounter = 0;
+        int counter = 1;
+        Arrays.sort(a);
+        for(int i = 1; i < a.length; i++) {
+            if (a[i] == a[i - 1])
+                counter++;
+            else {
+                if (counter % 3 == 0)
+                    finalCounter++;
+                counter = 1;
+            }
+        }
+        if (counter % 3 == 0)
+            finalCounter++;
+        System.out.println(finalCounter);
+    }
 }
